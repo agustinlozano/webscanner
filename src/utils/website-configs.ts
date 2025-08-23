3838dbb9080641e976a3d76582e15d0cca85ff07
Agustin B. Lozano <agustinlozanoblua@gmail.com>
2025-08-23 19:10:19 -0300

    Update websiteConfigs to include new scraping target and keywords for economic data

--- src/utils/website-configs.ts
+++ src/utils/website-configs.ts
@@ -2,33 +2,44 @@ import type { ScrapingConfig } from "@/scraper/scraper-service";
 
 export const websiteConfigs: ScrapingConfig[] = [
   {
-    url: "https://x.com/EmbAustraliaBA",
-    name: "Australian Embassy in Argentina - Twitter",
-    customDelay: 3000, // Wait for Twitter to load content
+    url: "https://www.lamacro.ar/variables",
+    name: "Variables económicas, monetarias y cambiarias del Banco Central de la República Argentina.",
     keywords: [
-      "subclass 462",
-      "work and holiday",
-      "visa",
-      "argentina",
-      "application",
-      "opening",
-      "closing",
-      "australia",
+      "dólar",
+      "inflación",
+      "tasa de interés",
+      "Reservas Internacionales",
+      "Tipo de cambio mayorista",
     ],
   },
-  {
-    url: "https://immi.homeaffairs.gov.au/news-media/archive",
-    name: "Australian Immigration News Archive",
-    customDelay: 2000, // Wait for page loader
-    keywords: [
-      "subclass 462",
-      "work and holiday",
-      "visa",
-      "argentina",
-      "working holiday maker",
-      "application",
-      "changes",
-      "update",
-    ],
-  },
+  // {
+  //   url: "https://x.com/EmbAustraliaBA",
+  //   name: "Australian Embassy in Argentina - Twitter",
+  //   customDelay: 3000, // Wait for Twitter to load content
+  //   keywords: [
+  //     "subclass 462",
+  //     "work and holiday",
+  //     "visa",
+  //     "argentina",
+  //     "application",
+  //     "opening",
+  //     "closing",
+  //     "australia",
+  //   ],
+  // },
+  // {
+  //   url: "https://immi.homeaffairs.gov.au/news-media/archive",
+  //   name: "Australian Immigration News Archive",
+  //   customDelay: 2000, // Wait for page loader
+  //   keywords: [
+  //     "subclass 462",
+  //     "work and holiday",
+  //     "visa",
+  //     "argentina",
+  //     "working holiday maker",
+  //     "application",
+  //     "changes",
+  //     "update",
+  //   ],
+  // },
 ];
